e6835c0e48e83f8cdc69a00935cf9f1bf876ce38
Shivaninaroju <shivaninaroju1@gmail.com>
2025-12-27 20:50:49 +0530

    GenAI

---
+++ llm.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY
+});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Hello there!"
+  });
+  console.log(response);
+}
+
+main();
+
+
